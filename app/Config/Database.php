3191707785fe9c54bcf16854e01735c3267ce2d2
JJenus <74376773+JJenus@users.noreply.github.com>
2021-04-01 17:49:20 +0100

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => '',
 		'username' => '',
 		'password' => '',
-		'database' => 'hill.db',
+		'database' => 'https://jtransact.000webhostapp.com/inc/hill.db',
 		'DBDriver' => 'SQLite3',
 		'DBPrefix' => '',
 		'pConnect' => false,
